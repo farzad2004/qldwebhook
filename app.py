f5830fc0800b45b500a6803ddf68a739355c4978
farzad <farzad.alamdara@intelliscape.com.au>
2018-01-18 04:37:55 +1100

    chnage

--- app.py
+++ app.py
@@ -4,23 +4,34 @@ import json
 import os
 import threading
 import urlparse
-
-from dropbox.client import DropboxClient, DropboxOAuth2Flow
+# from dropbox.client import DropboxClient, DropboxOAuth2Flow
 from flask import abort, Flask, redirect, render_template, request, session, url_for
 from markdown import markdown
 import redis
+import dropbox
+
  
+# redis_url = 'redis://redistogo:5e34518f7bf5e5dbeddec2493ad5c4a9@crestfish.redistogo.com:10389/'
+# redis_client = redis.from_url(redis_url)
+#
+# # App key and secret from the App console (dropbox.com/developers/apps)
+# APP_KEY = '2vxsimcvqyj99pz'
+# APP_SECRET = 'ntwalv1djb0uerj'
+
 redis_url = os.environ['REDISTOGO_URL']
 redis_client = redis.from_url(redis_url)
- 
+
 # App key and secret from the App console (dropbox.com/developers/apps)
 APP_KEY = os.environ['APP_KEY']
 APP_SECRET = os.environ['APP_SECRET']
- 
+
+
 app = Flask(__name__)
 app.debug = True
  
 # A random secret used by Flask to encrypt session data cookies
+# app.secret_key = 'a8c0a7117e071d5d50cda63b51405ed5eaefd78477a5d4fd553142ecc14333b2'
+
 app.secret_key = os.environ['FLASK_SECRET_KEY']
 
 def get_url(route):
@@ -34,13 +45,13 @@ def get_url(route):
 
     return url
 
-def get_flow():
-    return DropboxOAuth2Flow(
-        APP_KEY,
-        APP_SECRET,
-        get_url('oauth_callback'),
-        session,
-        'dropbox-csrf-token')
+# def get_flow():
+#     return DropboxOAuth2Flow(
+#         APP_KEY,
+#         APP_SECRET,
+#         get_url('oauth_callback'),
+#         session,
+#         'dropbox-csrf-token')
 
 @app.route('/welcome')
 def welcome():
@@ -51,48 +62,56 @@ def welcome():
 def oauth_callback():
     '''Callback function for when the user returns from OAuth.'''
 
-    access_token, uid, extras = get_flow().finish(request.args)
- 
+    # access_token, uid, extras = get_flow().finish(request.args)
+
     # Extract and store the access token for this user
-    redis_client.hset('tokens', uid, access_token)
+    # redis_client.hset('tokens', uid, access_token)
 
-    process_user(uid)
+    # process_user(uid)
+    token = 'Gq8-0VIcraAAAAAAAACh3NVW7B-8XDVBBbjbozCkhnlrhkM5rQDDyY8Sjn5ChyZr'
 
+    dbx = dropbox.Dropbox('Gq8-0VIcraAAAAAAAACh3NVW7B-8XDVBBbjbozCkhnlrhkM5rQDDyY8Sjn5ChyZr')
+    account = dbx.users_get_current_account()._account_id_value
+    redis_client.hset('tokens', account, token)
+    process_user(account)
     return redirect(url_for('done'))
 
-def process_user(uid):
-    '''Call /delta for the given user ID and process any changes.'''
+def process_user(account):
+    '''Call /files/list_folder for the given user ID and process any changes.'''
 
     # OAuth token for the user
-    token = redis_client.hget('tokens', uid)
+    token = redis_client.hget('tokens', account)
 
-    # /delta cursor for the user (None the first time)
-    cursor = redis_client.hget('cursors', uid)
+    # cursor for the user (None the first time)
+    cursor = redis_client.hget('cursors', account)
 
-    client = DropboxClient(token)
+    dbx = dropbox.Dropbox(token)
     has_more = True
 
     while has_more:
-        result = client.delta(cursor)
+        if cursor is None:
+            result = dbx.files_list_folder(path='')
+        else:
+            result = dbx.files_list_folder_continue(cursor)
 
-        for path, metadata in result['entries']:
-
+        for entry in result.entries:
             # Ignore deleted files, folders, and non-markdown files
-            if (metadata is None or
-                    metadata['is_dir'] or
-                    not path.endswith('.md')):
+            if (isinstance(entry, dropbox.files.DeletedMetadata) or
+                isinstance(entry, dropbox.files.FolderMetadata) or
+                not entry.path_lower.endswith('.md')):
                 continue
 
             # Convert to Markdown and store as <basename>.html
-            html = markdown(client.get_file(path).read())
-            client.put_file(path[:-3] + '.html', html, overwrite=True)
+            _, resp = dbx.files_download(entry.path_lower)
+            html = markdown(resp.content)
+            dbx.files_upload(html, entry.path_lower[:-3] + '.html', mode=dropbox.files.WriteMode.overwrite)
 
         # Update cursor
-        cursor = result['cursor']
-        redis_client.hset('cursors', uid, cursor)
+        cursor = result.cursor
+        redis_client.hset('cursors', account, cursor)
 
         # Repeat only if there's more to do
-        has_more = result['has_more']
+        has_more = result.has_more
 
 @app.route('/')
 def index():
@@ -100,7 +119,8 @@ def index():
 
 @app.route('/login')
 def login():
-    return redirect(get_flow().start())
+    # return '' redirect(get_flow().start())
+    return ''
 
 @app.route('/done')
 def done(): 
@@ -114,8 +134,8 @@ def validate_request():
     return signature == hmac.new(APP_SECRET, request.data, sha256).hexdigest()
 
 @app.route('/webhook', methods=['GET'])
-def challenge():
-    '''Respond to the webhook challenge (GET request) by echoing back the challenge parameter.'''
+def verify():
+    '''Respond to the webhook verification (GET request) by echoing back the challenge parameter.'''
 
     return request.args.get('challenge')
 
@@ -124,15 +144,19 @@ def webhook():
     '''Receive a list of changed user IDs from Dropbox and process each.'''
 
     # Make sure this is a valid request from Dropbox
-    if not validate_request(): abort(403)
+    signature = request.headers.get('X-Dropbox-Signature')
+    if not hmac.compare_digest(signature, hmac.new(APP_SECRET, request.data, sha256).hexdigest()):
+        abort(403)
 
-    for uid in json.loads(request.data)['delta']['users']:
+    for account in json.loads(request.data)['list_folder']['accounts']:
         # We need to respond quickly to the webhook request, so we do the
         # actual work in a separate thread. For more robustness, it's a
         # good idea to add the work to a reliable queue and process the queue
         # in a worker process.
-        threading.Thread(target=process_user, args=(uid,)).start()
+        threading.Thread(target=process_user, args=(account,)).start()
     return ''
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
